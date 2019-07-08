feb2f8ee8fa03821b251f1d8d70e7ac70046dd6e
SatooMasaya <36919316+SatooMasaya@users.noreply.github.com>
2019-07-08 14:54:41 +0900

    section01

---
+++ section01/Main0102.java
@@ -0,0 +1,17 @@
+package section01;
+
+/*
+ * 長方形の面積を計算して、出力するクラス
+ */
+
+public class Main0102 {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = a * b;
+
+        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+    }
+
+}
